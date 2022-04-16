ec9b3e22a4ddcb2c5ae14af69b22022ffd16f4e6
grantgariepy <grantgariepy@protonmail.com>
2022-04-16 11:46:19 -0600

    sidebar and navbar done start on first section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
+import Home from './pages'
 import './App.css';
 import {BrowserRouter as Router, } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
-      <Sidebar />
-      <Navbar />
+      <Home />
     </Router>
   );
 }
