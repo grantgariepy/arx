aa284e0476b3f23568918b9f36336aeeb31839a4
grantgariepy <grantgariepy@protonmail.com>
2022-04-16 10:03:35 -0600

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import './App.css';
+import {BrowserRouter as Router, } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements';
+
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">ARX</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
